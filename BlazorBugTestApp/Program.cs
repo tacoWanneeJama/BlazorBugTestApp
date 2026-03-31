1b891c27151aa50b892f3dd69adb023a872d404d
Taco Wannee <twa@jama.nl>
2026-03-31 16:51:04 +0200

    DatePicker werkend

--- BlazorBugTestApp/Program.cs
+++ BlazorBugTestApp/Program.cs
@@ -1,11 +1,21 @@
-using BlazorBugTestApp.Client.Pages;
 using BlazorBugTestApp.Components;
+using Blazorise;
+using Blazorise.Bootstrap5;
+using Blazorise.Icons.FontAwesome;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveWebAssemblyComponents();
+
+builder.Services
+    .AddBlazorise(options =>
+    {
+        options.Immediate = true;
+    })
+    .AddBootstrap5Providers()
+    .AddFontAwesomeIcons();
 
 var app = builder.Build();
 
