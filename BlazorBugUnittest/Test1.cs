5dcc2dfc7fed57287602735ed57ae269c1422ff1
Taco Wannee <twa@jama.nl>
2026-07-13 16:12:56 +0200

    Unittest added

---
+++ BlazorBugUnittest/Test1.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace BlazorBugUnittest
+{
+    public sealed class Test1
+    {
+        private static VerifySettings _verifySettings = new VerifySettings();
+
+        [ModuleInitializer]
+        public static void Initialize()
+        {
+            _verifySettings.UseUniqueDirectory();
+            if (!VerifyDiffPlex.Initialized)
+                VerifyDiffPlex.Initialize(VerifyTests.DiffPlex.OutputType.Compact);
+            Verifier.UseSourceFileRelativeDirectory("Snapshots");
+        }
+
+        [Test]
+        public async Task TestMethod1()
+        {
+            var testText = "Hello, World!";
+            await Verify(testText, _verifySettings);
+        }
+    }
+}
