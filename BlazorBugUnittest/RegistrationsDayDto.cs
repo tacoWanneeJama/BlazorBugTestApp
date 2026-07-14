28044e283f9947dfdf8d0e0f8e705ae892dab7f7
Taco Wannee <twa@jama.nl>
2026-07-14 11:19:12 +0200

    Finally reproducing the problem

---
+++ BlazorBugUnittest/RegistrationsDayDto.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorBugUnittest
+{
+    public class RegistrationsDayDto
+    {
+        public DateOnly Date { get; set; }
+        public int Count { get; set; }
+    }
+}
--- BlazorBugUnittest/Test1.cs
+++ BlazorBugUnittest/Test1.cs
@@ -1,4 +1,7 @@
 ﻿using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
 
 namespace BlazorBugUnittest
 {
@@ -11,15 +14,47 @@ namespace BlazorBugUnittest
         {
             _verifySettings.UseUniqueDirectory();
             if (!VerifyDiffPlex.Initialized)
-                VerifyDiffPlex.Initialize(VerifyTests.DiffPlex.OutputType.Compact);
+                VerifyDiffPlex.Initialize();
             Verifier.UseSourceFileRelativeDirectory("Snapshots");
         }
 
         [Test]
-        public async Task TestMethod1()
+        [Arguments("2025-01-04 03:00:00", "004000000603DC41")]
+        [Arguments("2026-05-16 03:00:00", "0040000014824483")]
+
+        public async Task TestMethod1(DateTime date, string tag)
         {
             var testText = "Hello, World!";
-            await Verify(testText, _verifySettings);
+            List<RegistrationsDayDto> testList = new() {
+                new RegistrationsDayDto { Date = DateOnly.FromDateTime(date), Count = 42 },
+                new RegistrationsDayDto { Date = DateOnly.FromDateTime(date), Count = 43 }
+            };
+
+            var hashDb = CalcualteObjectHash(tag);
+            await Assert.That(testList).IsNotNull();
+            var settings = GetVerifySettings();
+            await Verify(testList, settings);
+        }
+
+        public static VerifySettings GetVerifySettings()
+        {
+            var settings = new VerifySettings();
+            settings.UseUniqueDirectory();
+            //settings.UseDirectory("C:\\Temp");
+            return settings;
+        }
+
+        private string CalcualteObjectHash(object obj)
+        {
+            string jsonString = JsonSerializer.Serialize(obj);
+            byte[] jsonBytes = Encoding.ASCII.GetBytes(jsonString);
+
+            using (SHA256 mySHA256 = SHA256.Create())
+            {
+                mySHA256.ComputeHash(jsonBytes);
+                string hashString = Convert.ToBase64String(mySHA256.Hash);
+                return hashString;
+            }
         }
     }
 }
