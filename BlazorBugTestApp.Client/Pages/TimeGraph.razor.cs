c52e77505a01eed686cffb0a685ff2c76fa5bac6
Floris-D <fjd@jama.nl>
2026-07-17 16:57:55 +0200

    Commit

--- BlazorBugTestApp.Client/Pages/TimeGraph.razor.cs
+++ BlazorBugTestApp.Client/Pages/TimeGraph.razor.cs
@@ -1,29 +1,66 @@
 ﻿using Microsoft.AspNetCore.Components;
+using System.Dynamic;
 
 namespace BlazorBugTestApp.Client.Pages
 {
     [Route("timeGraph")]
     public partial class TimeGraph
     {
-        private List<(int Value, DateTime Time)> _data = new()
-            {
-                (1, DateTime.Parse("04:00:00")),
-                (2, DateTime.Parse("04:25:00")),
-                (3, DateTime.Parse("04:30:09")),
-                (4, DateTime.Parse("04:55:38")),
-                (5, DateTime.Parse("05:11:11")),
-                (6, DateTime.Parse("05:44:23")),
-                (7, DateTime.Parse("06:28:34")),
-                (8, DateTime.Parse("06:30:00")),
-                (9, DateTime.Parse("06:45:02")),
-                (10, DateTime.Parse("07:02:00")),
-                (11, DateTime.Parse("07:08:12")),
-                (12, DateTime.Parse("07:30:00")),
-                (13, DateTime.Parse("07:33:00")),
-                (14, DateTime.Parse("08:04:22")),
-                (15, DateTime.Parse("08:22:45"))
-            };
+        private List<DateTime> _listTime = new()
+        {
+            DateTime.Parse("04:00:00"),
+            DateTime.Parse("04:25:00"),
+            DateTime.Parse("04:30:09"),
+            DateTime.Parse("04:55:38"),
+            DateTime.Parse("05:11:11"),
+            DateTime.Parse("05:44:23"),
+            DateTime.Parse("06:28:34"),
+            DateTime.Parse("06:30:00"),
+            DateTime.Parse("06:45:02"),
+            DateTime.Parse("07:02:00"),
+            DateTime.Parse("07:08:12"),
+            DateTime.Parse("07:30:00"),
+            DateTime.Parse("07:33:00"),
+            DateTime.Parse("08:04:22"),
+            DateTime.Parse("08:22:45")
+        };
+        private List<double> _listValues = new()
+        {
+            22.55,
+            33.45,
+            19.10,
+            18.00,
+            8.30,
+            36.50,
+            10.25,
+            14.75,
+            28.20,
+            31.00,
+            9.35,
+            7.85,
+            40.65,
+            39.95,
+            9.95
+        };
+        private List<(DateTime, double)> _listCombined = new();
 
+        public void ReadTimes()
+        {
+            foreach(var time in _listTime)
+            {
+                Console.WriteLine(time.ToOADate().ToString());
+            }
+        }
+
+        public List<(DateTime, double)> CombineList()
+        {
+            _listCombined.Clear();
+            for (int i = 1; i <= _listTime.Count(); i++)
+            {
+                _listCombined.Add((_listTime[i], _listValues[i]));
+            }
+            return _listCombined;
+        }
         public List<(int Value, DateTime Time)> GetDataWithLocalTime()
         {
             List<(int Value, DateTime Time)> tempList = new();
